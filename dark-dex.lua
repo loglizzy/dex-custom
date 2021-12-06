9c2ac454535b58648250445d263798f8de732cfb
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-12-05 20:47:57 -0400

    Update and rename synapse.lua to dark-dex.lua

---
+++ dark-dex.lua
@@ -0,0 +1,9 @@
+Icon_Color_Theme = 'Graphite'
+
+-- Themes:
+--   Graphite (dark)
+--   Platinium (light)
+--   White (full white icons)
+
+shared['391D941U3418SK'],shared['IDFI139F4O312'] = Icon_Color_Theme,'8186928651'
+loadstring(game:HttpGet('https://raw.githubusercontent.com/loglizzy/dex-custom/main/loaders/dark-dex.lua'))()
