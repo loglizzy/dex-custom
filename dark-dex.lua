53b14027c8475865d859e00dd3a733329bb66741
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-12-05 21:04:36 -0400

    Update dark-dex.lua

--- dark-dex.lua
+++ dark-dex.lua
@@ -5,5 +5,5 @@ Icon_Color_Theme = 'Graphite'
 --   Platinium (light)
 --   White (full white icons)
 
-shared['391D941U3418SK'],shared['IDFI139F4O312'] = Icon_Color_Theme,'8187094582'
+shared['391D941U3418SK'],shared['IDFI139F4O312'] = Icon_Color_Theme,'8187023605'
 loadstring(game:HttpGet('https://raw.githubusercontent.com/loglizzy/dex-custom/main/loaders/darkdex.lua'))()
